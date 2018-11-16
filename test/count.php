f06550bd49de3c4b113a81a6d1be600530856031
Sébastien de Beauffort <sewelseb@hotmail.com>
2018-11-16 04:43:05 +0100

    count

---
+++ test/count.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // echo __DIR__;
+        $directory = opendir(__DIR__);
+        $total = 0;
+        while($file = readdir($directory)){
+            if(is_file($file)){
+                echo $file.": ".filesize($file)."charcaters<br />";
+                $total += filesize($file);
+            }
+        }
+        echo "total : ".$total."characters";
+            
+    ?>
+</body>
+</html>
---
+++ test/mesnu.php
@@ -0,0 +1,6 @@
+<div>
+    item 1<br/>
+    item 2<br/>
+    item 3 <br/>
+    item 4<br/>
+</div>
